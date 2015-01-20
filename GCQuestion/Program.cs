0965a52363d8e46dc109427fabded594409d8ed0
Gábor Plesz <gabor.plesz@gmail.com>
2015-01-20 12:27:09 +0100

    initial commit of source

---
+++ GCQuestion/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var max = 1;
+
+            for (int i = 0; i < max; i++)
+            {
+                var inherited = new GCInherited(i);
+
+                //if explicit set to null, the finalizer started:
+                //inherited = null;
+            }
+            GC.Collect(2);
+            GC.WaitForPendingFinalizers();
+            GC.WaitForFullGCApproach(-1);
+            GC.WaitForFullGCComplete(-1);
+            Console.ReadLine();
+        }
+    }
+
+    class GCInherited : GCBase
+    {
+        public GCInherited(int nr) : base(nr) { }
+
+        ~GCInherited() { Console.WriteLine("GCInherited finalizer ({0})", this.nr); }
+    }
+
+    class GCBase
+    {
+        public int nr { get; private set; }
+
+        public GCBase(int nr) { this.nr = nr; }
+
+        ~GCBase() { Console.WriteLine("GCBase finalizer"); }
+    }
+}
